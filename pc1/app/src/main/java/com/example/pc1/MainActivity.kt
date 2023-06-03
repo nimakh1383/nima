774d8a7fef1a99d5903efd795bb7efde265c8ec5
nimakh1383 <khodadadynima677@gmail.com>
2023-06-03 21:26:42 +0330

    MyBottomNavigation

---
+++ pc1/app/src/main/java/com/example/pc1/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.pc1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Surface
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pc1.ui.theme.Pc1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Pc1Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(){
+    val navController = rememberNavController()
+    Scaffold(bottomBar = { MyBottomNavigation(navController = navController)}) {
+        Box(modifier = Modifier.padding(it)){
+            NavHost(navController = navController, startDestination = Login.Route ){
+                composable(Login.Route){
+                    login(navController)
+                }
+                composable(Welcome.Route){
+                    Welcome
+                }
+            }
+        }
+    }
+}
---
+++ pc1/app/src/main/java/com/example/pc1/ui/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.pc1.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xE186FCE4)
+val Purple500 = Color(0xBCEECE00)
+val Purple702 = Color(0xFF00B398)
+val Teal201 = Color(0xFFABDA03)
